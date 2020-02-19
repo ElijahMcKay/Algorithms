24615e4168bbfb163f4b2281e185b44aafcd011e
ElijahMcKay <elijahmckay10@gmail.com>
2020-02-19 14:17:12 -0600

    Completed stock_prices.  Going to find better solution after I finish the other ones

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -2,8 +2,21 @@
 
 import argparse
 
+stocks = [1050, 270, 1540, 3800, 2]
+
 def find_max_profit(prices):
-  pass
+	# defining a variable to contain our max profit so far.  Find a more elegant solution for this
+	max_profit = -10000
+	for i in range(len(prices)):
+		# looping through each value after the current (we can't compare to values that have already passed)
+		for j in range(i + 1, len(prices)):
+			if prices[j] - prices[i] > max_profit:
+				max_profit = prices[j] - prices[i]
+
+	return max_profit
+
+
+print(find_max_profit(stocks))
 
 
 if __name__ == '__main__':
